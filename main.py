69cde0c483059f0be5d9c775f0c088c81ce1a8b3
ayah alsharabi <ayahalsharabi@MacBook-ayah.local>
2021-04-10 21:05:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+num = int(input('Enter the prime number-'))
+N = 2
+while num!= 0:
+    for i in range(2, N):
+        if N%i== 0:
+            break
+    else:
+        print(N,end=" ")
+        num -= 1
+    N += 1
+
+
